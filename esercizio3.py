3a6e02601f68baa92d1ed9b4ca310b2c6487fc65
Gaia Cassese <115220977+gaiacassese@users.noreply.github.com>
2022-11-22 12:35:01 +0000

    VI commit

---
+++ esercizio3.py
@@ -0,0 +1,29 @@
+class CSVfile():
+    def __init__(self, name):
+        self.name= name
+
+    def get_data():
+        def sum_csv(file_name):
+            res = 0
+            my_file = open(file_name, 'r')
+
+            if my_file==[]:
+                return None
+    
+            for line in my_file:
+
+                elements = line.split(',')
+                if elements[0] != 'Date':
+ 
+                    try: 
+                        value=float(elements[1])
+                        res=res+value
+            
+                    except:
+                        value=None
+            
+            if res==0:
+                return None
+               
+            return res 
+
