c4735582451b9d7b35b2ef99a83a0249a7c6e03a
Gaia Cassese <115220977+gaiacassese@users.noreply.github.com>
2022-11-24 13:15:31 +0000

    VI

--- esercizio3.py
+++ esercizio3.py
@@ -1,29 +1,23 @@
 class CSVfile():
     def __init__(self, name):
-        self.name= name
+        self.name= name #è una variabile dell'oggetto
+        
+#init è funzione standard che serve ad inizializzare l'oggetto
+    def get_data(self):
 
-    def get_data():
-        def sum_csv(file_name):
-            res = 0
-            my_file = open(file_name, 'r')
+        values=[]
+        my_file=open(self.name, 'r')
 
-            if my_file==[]:
-                return None
-    
-            for line in my_file:
+        if my_file == []:
+            return None
 
-                elements = line.split(',')
-                if elements[0] != 'Date':
- 
-                    try: 
-                        value=float(elements[1])
-                        res=res+value
-            
-                    except:
-                        value=None
-            
-            if res==0:
-                return None
-               
-            return res 
+        for line in my_file:
+            elements=line.split(',')
+            if elements[0]!= 'Date':
+                values.append(elements)
+        return values
 
+my_file = CSVfile('shampoo_sales.csv') #trasformo la classe in un oggetto, non posso usare le classi nel codice in quanto sono astratte
+#my_file=self.name
+my_file.get_data()#esegue le istruzioni di get_data
+print(my_file.get_data())
---
+++ lezione4.py
@@ -0,0 +1,18 @@
+class CSVFile():
+    
+    def __init__(self,name):
+        self.name = name
+
+    def get_data(self):
+        
+        values = []
+        my_file = open(self.name, 'r')
+        
+        if my_file == []:
+            return None
+
+        for line in my_file:
+            elements = line.split(',')
+            if elements[0] != 'Date':
+                values.append(elements)
+        return values
