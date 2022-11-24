a159c1d86540a6e7165bc534bbc34b9453ee3dff
Gaia Cassese <115220977+gaiacassese@users.noreply.github.com>
2022-11-24 13:33:39 +0000

    VIII

--- lezione4.py
+++ lezione4.py
@@ -1,13 +1,13 @@
 class CSVFile():
-    
-    def __init__(self,name):
+
+    def __init__(self, name):
         self.name = name
 
     def get_data(self):
-        
+
         values = []
         my_file = open(self.name, 'r')
-        
+
         if my_file == []:
             return None
 
@@ -15,5 +15,5 @@ class CSVFile():
             elements = line.strip('\n').split(',')
             if elements[0] != 'Date':
                 values.append(elements)
-                
+
         return values
