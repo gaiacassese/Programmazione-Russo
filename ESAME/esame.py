0090be3b4636dedc2f62d4c95d3c416e20f67615
Gaia Cassese <115220977+gaiacassese@users.noreply.github.com>
2023-02-23 10:35:40 +0000

    versione 1.1

--- ESAME/esame.py
+++ ESAME/esame.py
@@ -11,15 +11,16 @@ class CSVTimeSeriesFile:
         
         import datetime
 
-        # lista da restituire in caso di successo
+        #lista da restituire in caso di successo
         retList = []
         
-        # la userò per vedere se è ordinata o se ci sono duplicati
+        #userò questa lista per vedere se è ordinata o se ci sono duplicati
         listaMesiAnni = []
         
         #controllo se il nome è None
         if self.name is None:
             raise ExamException('Errore! nome file non inserito in input')
+            
         #prova ad aprire il file
         try:
             my_file = open(self.name,"r")
@@ -30,53 +31,51 @@ class CSVTimeSeriesFile:
             my_file.close()
             
         except Exception as e:
-            raise ExamException('File non trovato o non leggibile! errore: "{}" '.format(e))
+            raise ExamException('Errore! File non trovato o non leggibile: "{}" '.format(e))
  
         #adesso posso aprire il file 
             
         with open(self.name) as csv_file:
-
             for line in csv_file:
-                   
                 elements = line.strip("\n").split(",")
 
         #se manca un parametro salto la riga e continuo senza alzare eccezioni
                 if len(elements) < 2:
-                    print('manca almeno un parametro nella riga: --> "{}" - SKIP'.format(elements))
+                    print('Manca almeno un parametro nella riga: --> "{}" - SKIP'.format(elements))
                     continue
         #non serve che controlli se ci sono più di due elementi perchè la funzione split prende i primi due in automatico
                 
                 if elements[0] == "date":
                     #sono sull'intestazione
-                    print('salto intestazione --> "{}" '.format(elements))
+                    print('Salto intestazione --> "{}" '.format(elements))
                     continue
 
                 #per comodità assegno data e passeggeri a delle variabili locali per non lavorare con element[...]
                 data = elements[0]
                 
-                #controlla che la data sia valida, se non lo è salto la riga senza alzare eccezioni
+                #controllo che la data sia valida, se non lo è salto la riga senza alzare eccezioni
                 try:
                     testData = data.split("-")
-                    datetime.datetime(int(testData[0]),int(testData[1]),1)
+                    datetime.datetime(int(testData[0]),int(testData[1]),1) #metto giorno 1 per essere sicura che esista
                 except:
-                    print("impossibile convertire la data --> {} - SKIP".format(data))
+                    print("Impossibile convertire la data --> {} - SKIP".format(data))
                     continue
                     
-                #controlla che il numero di passeggeri sia valido, se non lo è salto la riga senza alzare eccezioni
+                #controllo che il numero di passeggeri sia valido, se non lo è salto la riga senza alzare eccezioni
                 try:
                     passeggeri = int(elements[1])
                 except:
-                    print("numero di passeggeri non numerico --> {} - SKIP".format(elements[1]))
+                    print("Il valore del numero di passeggeri non è numerico --> {} - SKIP".format(elements[1]))
                     continue
                 #controllo che il numero di passeggeri sia non negativo
                 if passeggeri < 0:
-                    print("il numero di passeggeri deve essere non negativo--> {} - SKIP".format(elements[1]))
+                    print("Il numero di passeggeri deve essere non negativo--> {} - SKIP".format(elements[1]))
                     continue
 
                 
                 #controllo che l'anno sia compreso tra 1914, anno in cui ha volato il primo aereo di linea, e l'anno attuale
                 if int(testData[0]) < 1914 or int(testData[0]) > datetime.date.today().year:
-                    print("l'anno non è corretto --> {} - SKIP".format(elements[0]))
+                    print("L'anno non è corretto --> {} - SKIP".format(elements[0]))
                     continue
 
                 #creo la sottolista (rowlist) che poi andrò ad inserire nella lista generale (retlist)
@@ -85,7 +84,7 @@ class CSVTimeSeriesFile:
                 retList.append(rowList)
                 listaMesiAnni.append(data)
 
-             # controllo se la lista è ordinata
+             #controllo se la lista è ordinata
             listaOrdinata = listaMesiAnni.copy()
             listaOrdinata.sort()
             
@@ -94,18 +93,18 @@ class CSVTimeSeriesFile:
                 print("La lista è ordinata")
             else:
                 #non ordinata
-                raise ExamException("la lista NON è ordinata")
+                raise ExamException("Errore! La lista NON è ordinata")
             
-            # controllo se la lista ha duplicati
+            #controllo se la lista ha duplicati
             lunghezzalista = len(listaMesiAnni)
             lunghezzalistaSet = len(set(listaMesiAnni))
             
             if lunghezzalista == lunghezzalistaSet:
-                # se la lunghezza della lista è uguale alla lista senza duplicati allora non ci sono duplicati
+                #se la lunghezza della lista è uguale alla lista senza duplicati allora non ci sono duplicati
                 print("La lista non contiene duplicati")
             else:
                 #altrimenti alzo un'eccezione
-                raise ExamException("La lista contiene dei duplicati")
+                raise ExamException("Errore! La lista contiene dei duplicati")
 
                 
         return retList
@@ -118,34 +117,34 @@ def detect_similar_monthly_variations(time_series, years):
     
     #controllo time_series
     if time_series is None:
-        print("lista time_series None")
-        raise ExamException("La lista time_series è None")
+        print("La lista time_series è None")
+        raise ExamException("Errore! La lista time_series è nulla")
         
     if len(time_series)==0:
-        print("lista time_series ha lunghezza 0")
-        raise ExamException("La lista time_series ha lunghezza 0")
+        print("La lista time_series ha lunghezza 0")
+        raise ExamException("Errore! La lista time_series ha lunghezza 0")
         
     #controllo years
     #se la lista è None
     if years is None:
-        print("lista anni None")
-        raise ExamException("La lista degli anni è None")
+        print("Lista anni None")
+        raise ExamException("Errore! La lista degli anni è None")
     
     #che la lista abbia 2 valori
     if len(years) != 2:
-        print("gli anni devono essere 2")
-        raise ExamException("gli anni devono essere 2")
+        print("Gli anni devono essere 2")
+        raise ExamException("Errore! Gli anni devono essere 2")
     
     # valori interi per gli anni
     if type(years[0]) is not int:
-        raise ExamException("il primo anno non è intero")
+        raise ExamException("Errore! Il primo anno non è intero")
         
     if type(years[1]) is not int:
-        raise ExamException("il secondo anno non è intero")
+        raise ExamException("Errore! Il secondo anno non è intero")
         
     # anni ordinati consecutivi
     if years[1] != years[0] + 1:
-        raise ExamException("il secondo anno deve essere successivo al primo")
+        raise ExamException("Errore! Il secondo anno deve essere successivo al primo")
     
     #anni nella lista
     for anno in years:
@@ -157,16 +156,16 @@ def detect_similar_monthly_variations(time_series, years):
                 break
     
         if not found:
-            raise ExamException('Anno non nella lista "{}"'.format(anno))
+            raise ExamException('Errore! Anno non nella lista "{}"'.format(anno))
 
-    # fine controlli, inizia l'algoritmo
+    #fine controlli, inizia l'algoritmo
 
     #uso questa nomenclatura per aggiungere direttamente il valore che voglio tramite indexing
             
     anno0 = [None,None,None,None,None,None,None,None,None,None,None,None]
     anno1 = [None,None,None,None,None,None,None,None,None,None,None,None]
 
-    # riempo gli array con delle variabili temporanee 
+    #riempo gli array con delle variabili temporanee 
     for el in time_series:
         tempAnno = int(el[0].split("-")[0])
         tempMese = int(el[0].split("-")[1])
@@ -179,8 +178,8 @@ def detect_similar_monthly_variations(time_series, years):
             anno1[tempMese-1] = tempPasseggeri
         
     # calcolo le differenze
-    # se qualcosa va storto... metto none (evidentemente manca)
-    #ci saranno 11 segnaposti in quanto la lista delle differenze avrà un posto in meno rispetto alla lista in cui vado a prendere i dati
+    #se qualcosa va storto... metto None (evidentemente manca)
+    #ci saranno 11 segnaposti in quanto la lista delle differenze avrà un posto in meno rispetto alla lista in cui vado a prendere i dati (le coppie sono 11)
     
     diff0 = [None,None,None,None,None,None,None,None,None,None,None]
     diff1 = [None,None,None,None,None,None,None,None,None,None,None]
@@ -196,17 +195,17 @@ def detect_similar_monthly_variations(time_series, years):
         except:
             diff1[x] = None
 
-    # finalmente calcolo il risultato della funzione
+    #finalmente calcolo il risultato della funzione
     
     retList=[False,False,False,False,False,False,False,False,False,False,False]
     
     for i in range(11):
-        # caso del none, resta falso
+        #caso del None, resta falso
         if diff1[i] == None or diff0[i] == None:
             retList[i] = False
             continue
         
-        # calcolo differenza
+        #calcolo differenza
         d = diff0[i] - diff1[i]
         if d >= -2 and d <= 2:
             retList[i] = True
@@ -223,5 +222,3 @@ time_series = time_series_file.get_data()
 years=[1949,1950]
 
 ritorno=detect_similar_monthly_variations(time_series, years)
-print(ritorno)
-print(years)
